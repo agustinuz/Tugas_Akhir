0abe0a71781f0a72f10cfa0656b3502628871959
Franstio <franstiodiego01@gmail.com>
2024-10-01 23:44:58 +0700

    ok

--- Backend/routers/index.js
+++ Backend/routers/index.js
@@ -36,6 +36,7 @@ import {
   getCart,
 } from "../controllers/Cart.js";
 import { getImageById, saveImage } from "../controllers/UserImage.js";
+import { CreateTransaksi, UpdateTransaksi } from "../controllers/Transaction.js";
 
 const router = express.Router();
 
@@ -71,4 +72,6 @@ router.delete("/cart/:userId", deleteCartUser);
 router.delete("/cart/:cartId", deleteCartItem);
 router.get("/profile-image/:userId", getImageById);
 router.post("/upload-profile-image", saveImage);
+router.post('/transaksi',CreateTransaksi);
+router.put('/transaksi/:id',UpdateTransaksi);
 export default router;
